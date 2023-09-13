906684081e63183b9a79841d73e598b69972785c
joao0812 <joaoesalmeida@hotmail.com>
2023-09-13 15:05:07 -0300

    fix: add delete cascade in company

--- src/repositories/Company/deleteCompany/mongo-delete-company.ts
+++ src/repositories/Company/deleteCompany/mongo-delete-company.ts
@@ -1,16 +1,21 @@
 import mongoose from "mongoose";
 import { IDeleteCompanyRepository } from "../../../controllers/Company/deleteCompany/protocols";
 import { Company, companyModel } from "../../../models/Company";
+import areaModel from "../../../models/Area";
+import positionModel from "../../../models/Positon";
 
 export class DeleteCompanyRepository implements IDeleteCompanyRepository {
     async removeCompany(id: string): Promise<Company> {
         const company = await companyModel.findOneAndDelete({_id: new mongoose.Types.ObjectId(id)}).lean()
-
+        
         if (!company){
             throw new Error('Erro to delete company')
         }
-
+        
         const {_id, ...rest} = company
+
+        const area = await areaModel.deleteMany({company_id: {_id: _id}})
+        const position = await positionModel.deleteMany({company_id: {_id: _id}})
 
         return {id: _id.toHexString(), ...rest}
     }
